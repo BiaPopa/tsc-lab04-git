13a6046d178088179c61d9eb2f9ef61f531ed90e
Bianca Popa <biancapopa2002@gmail.com>
2026-03-23 00:25:02 +0200

    Implement 5 random number generation methods

--- practice/main.cc
+++ practice/main.cc
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
 
-using namespace std;
+int main() {
+    std::srand(std::time(nullptr));
 
-int main()
-{
-    int n;
+    int r1 = std::rand();
 
-    cout << "Please enter a number" << endl;
-    cin >> n;
-    cout << "Your number + 5 is: " << n + 5 << endl;
+    long r2 = random();
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    int r3 = mt();
+
+    std::minstd_rand lcg(rd());
+    int r4 = lcg();
+
+    std::ranlux24_base ranlux(rd());
+    int r5 = ranlux();
+
+    std::cout << "Method 1 (rand): " << r1 << '\n';
+    std::cout << "Method 2 (random): " << r2 << '\n';
+    std::cout << "Method 3 (mt19937): " << r3 << '\n';
+    std::cout << "Method 4 (minstd_rand): " << r4 << '\n';
+    std::cout << "Method 5 (ranlux24_base): " << r5 << '\n';
 
     return 0;
 }
